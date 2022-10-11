e5599a99351c999a4d3648c11bce9178d08fe672
Jesse Mitchell-Reiss <j.mitchell-reiss@outlook.com>
2022-10-10 22:12:15 -0700

    remove tests

--- Assets/Player/Scene Manager/LevelManager.cs
+++ Assets/Player/Scene Manager/LevelManager.cs
@@ -7,9 +7,11 @@ using System;
 public class LevelManager : MonoBehaviour
 {
     /* 
-     * require menu scene to have build index 0, end scene to have build index 1, and all other scenes to have build
-     * indecies beginning at 2 LEVEL MANAGER WILL NOT FUNCTION WITHOUT THIS CONFIGURATION!!!!!
+     * All assets that are not C# scripts must be stored in asset bundles within the /Assets/StreamingAssets directory
+     * They must be bundled up into two separate bundles per student, one for the scene and one for assets
+     * All C# scripts must be stored in a separate folder and compiled with the project
      */
+
     int currentHole;
     int holeCount;
     List<Scene> allHoles;
@@ -17,13 +19,31 @@ public class LevelManager : MonoBehaviour
 
     public void gameStart(int holes = 9)
     {
-        //load all asset bundless and append to list
-        List<AssetBundle> bundles = new List<AssetBundle>(System.IO.Directory.GetFiles(Application.streamingAssetsPath).Length);
-        int iterator = 0;
+        // load all asset bundless and append to list
+        // determine number of total asset bundles
+        // create lists for scenes and other
+        List<AssetBundle> sceneBundles = new List<AssetBundle>(System.IO.Directory.GetFiles(Application.streamingAssetsPath).Length / 4);
+        List<AssetBundle> otherBundles = new List<AssetBundle>(System.IO.Directory.GetFiles(Application.streamingAssetsPath).Length / 4); 
+        int sceneBundleIterator = 0;
+        int otherBundleIterator = 0;
+        // assign each bundle to an index
         foreach(string i in System.IO.Directory.GetFiles(Application.streamingAssetsPath))
         {
-            bundles[iterator] = AssetBundle.LoadFromFile(i);
-            iterator++;
+            // check for not meta files
+            if(!i.Contains(".meta"))
+            {
+                // separate bundles into scenes and other
+                if(i.Contains("scene"))
+                {
+                    sceneBundles[sceneBundleIterator] = AssetBundle.LoadFromFile(i);
+                    sceneBundleIterator++;
+                }
+                else
+                {
+                    otherBundles[otherBundleIterator] = AssetBundle.LoadFromFile(i);
+                    otherBundleIterator++;
+                }
+            }
         }
         // get scenes and add to allHoles
         int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
